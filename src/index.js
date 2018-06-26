be69f77989e31a853922da9d2bbc9e7e087b2eb1
Tim Kindberg <timkindberg@gmail.com>
2018-06-26 11:38:55 -0400

    Remove the word 'Poop'

--- src/index.js
+++ src/index.js
@@ -3,20 +3,20 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-class App extends React.Component { 
+class App extends React.Component {
   state = {
     showPoop: false
-  }
+  };
   render() {
     return (
       <div className="App">
-        <h1 onClick={() => this.setState({showPoop: !this.state.showPoop})}>
-          Hello Poopface {this.state.showPoop ? '💩' : ''}
+        <h1 onClick={() => this.setState({ showPoop: !this.state.showPoop })}>
+          Hello World {this.state.showPoop ? "💩" : ""}
         </h1>
         <h2>Start editing to see some poop happen!</h2>
       </div>
-    )
-  };
+    );
+  }
 }
 
 const rootElement = document.getElementById("root");
